9bff307d6f0c81a5e1c4190ee03a64767c188b12
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-07-31 09:54:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE>
 <html>
 <head>
-
+<body style="background:skyblue;">
 <title>Products</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
