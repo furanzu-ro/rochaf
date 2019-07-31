dd7b3a10f6d3c3515456f667573b1376e58faa8f
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-07-31 10:06:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,42 @@
-  color: white;
-  font-size: 30px;
+ <!DOCTYPE>
+<html>
+<head>
+<body style="background:skyblue;">
+<title>Products</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+font-family: Arial;
+margin: 0;
 }
-
+.header {
+padding: 60px;
+text-align: center;
+background: red;
+color: red;
+font-size: 30px;
+}
 .content {padding:20px;}
-.footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   background-color: black;
-   color: white;
-   text-align: center;
-}
 </style>
 </head>
 <body>
-
-<div class="footer">
-  <p>Footer</p>
-</div>
-<div class="header">
-  <h1>Franz.com</h1>
-  <p>Header ni siya</p>
-</div>
-
-<div class="content">
-<center>  
-<h1>This is Franz!</h1>
-  <p>This is my property</p>
-</center>
-</div>
-
-
+<center> <img src = "https://laz-img-cdn.alicdn.com/images/ims-web/TB19SB7aMFY.1VjSZFnXXcFHXXa.png"/> </center>
+<hr>
+</hr>
+<a href="https://www.w3schools.com/html/html_links.asp/"> Devices > Gaming</a> <p1>> Consoles</p>
+<center> <img src= "https://target.scene7.com/is/image/Target/GUEST_3013eeb4-a862-402a-90e5-9b52fdf1e364?wid=488&hei=488&fmt=pjpeg" style="width:400px;height:300px;"/> </center>
+<h4 align = "center"> <u> Nintendo Switch with Neon Blue and Neon Red Joy-Con </u> </h4>
+<h3> PHP 20,500.00 </h3>
+<p1> Details about this product: </p1>
+<ul>
+<li> Brand New </li>
+<li> gives you total gameplay flexibility </li>
+<li> Compete against friends </li>
+<li> Play online with other gamers</li>
+<li> Link up to eight systems together to play anywhere</li>
+</ul>
+<center> <button> Buy </button> <button> Wish List </button> </center>
+<p> Thank you for reading!</p1>
 </body>
 </html>
